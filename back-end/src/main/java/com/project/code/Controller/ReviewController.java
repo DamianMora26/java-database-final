3888493b7aac9f1403f9e5becabf314140c304ae
DamianMora26 <damianuzer12345@gmail.com>
2026-09-20 14:17:22 -0700

    Simplify imports and autowired annotations in ReviewController
    Refactored imports and removed redundant annotations for ReviewController.

--- back-end/src/main/java/com/project/code/Controller/ReviewController.java
+++ back-end/src/main/java/com/project/code/Controller/ReviewController.java
@@ -1,23 +1,15 @@
 package com.project.code.Controller;
-
-import com.project.code.Model.Customer;
-import com.project.code.Model.Review;
-import com.project.code.Repository.CustomerRepository;
-import com.project.code.Repository.ReviewRepository;
+import com.project.code.Model.*;
+import com.project.code.Repo.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.*;
 
 @RestController
 @RequestMapping("/reviews")
 public class ReviewController {
-
-    @Autowired
-    private ReviewRepository reviewRepository;
-
-    @Autowired
-    private CustomerRepository customerRepository;
+    @Autowired private ReviewRepository reviewRepository;
+    @Autowired private CustomerRepository customerRepository;
 
     @GetMapping
     public Map<String, Object> getAllReviews() {
@@ -31,23 +23,18 @@ public class ReviewController {
         Map<String, Object> response = new HashMap<>();
         List<Review> reviews = reviewRepository.findByStoreIdAndProductId(storeId, productId);
         List<Map<String, Object>> formattedReviews = new ArrayList<>();
-
         for (Review review : reviews) {
             Map<String, Object> reviewMap = new HashMap<>();
             reviewMap.put("comment", review.getComment());
             reviewMap.put("rating", review.getRating());
-
             String customerName = "Desconocido";
             if (review.getCustomerId() != null) {
                 Customer customer = customerRepository.findById(review.getCustomerId());
-                if (customer != null && customer.getName() != null) {
-                    customerName = customer.getName();
-                }
+                if (customer != null && customer.getName() != null) customerName = customer.getName();
             }
             reviewMap.put("customerName", customerName);
             formattedReviews.add(reviewMap);
         }
-
         response.put("reviews", formattedReviews);
         return response;
     }

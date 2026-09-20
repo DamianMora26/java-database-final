f7b65756b892c6dbb8a6a2606dcdddea4c353778
DamianMora26 <damianuzer12345@gmail.com>
2026-09-20 11:59:47 -0700

    Add method to retrieve all reviews

--- back-end/src/main/java/com/project/code/Controller/ReviewController.java
+++ back-end/src/main/java/com/project/code/Controller/ReviewController.java
@@ -19,6 +19,11 @@ public class ReviewController {
     @Autowired
     private CustomerRepository customerRepository;
 
+    @GetMapping
+    public List<Review> getAllReviews() {
+        return reviewRepository.findAll();
+    }
+
     @GetMapping("/{storeId}/{productId}")
     public Map<String, Object> getReviews(@PathVariable Long storeId, @PathVariable Long productId) {
         Map<String, Object> response = new HashMap<>();

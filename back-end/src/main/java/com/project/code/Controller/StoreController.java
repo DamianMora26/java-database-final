859a9e1d6024709219fae0984b9f550085a95eaa
DamianMora26 <damianuzer12345@gmail.com>
2026-09-20 12:21:10 -0700

    Change return types to ResponseEntity in StoreController

--- back-end/src/main/java/com/project/code/Controller/StoreController.java
+++ back-end/src/main/java/com/project/code/Controller/StoreController.java
@@ -5,6 +5,8 @@ import com.project.code.Repository.StoreRepository;
 import com.project.code.Service.OrderService;
 import com.project.code.dto.PlaceOrderRequestDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -22,34 +24,41 @@ public class StoreController {
     private OrderService orderService;
 
     @PostMapping
-    public Map<String, String> addStore(@RequestBody Store store) {
+    public ResponseEntity<Map<String, String>> addStore(@RequestBody Store store) {
         Store savedStore = storeRepository.save(store);
         Map<String, String> response = new HashMap<>();
         response.put("message", "Tienda creada con éxito con ID: " + savedStore.getId());
-        return response;
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
     @GetMapping("validate/{storeId}")
-    public boolean validateStore(@PathVariable Long storeId) {
+    public ResponseEntity<Boolean> validateStore(@PathVariable Long storeId) {
         Optional<Store> store = storeRepository.findById(storeId);
-        return store.isPresent();
+        if (store.isPresent()) {
+            return new ResponseEntity<>(true, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(false, HttpStatus.NOT_FOUND);
     }
 
     @GetMapping("validate/store/{storeId}")
-    public boolean validateStoreAlt(@PathVariable Long storeId) {
+    public ResponseEntity<Boolean> validateStorePath(@PathVariable Long storeId) {
         Optional<Store> store = storeRepository.findById(storeId);
-        return store.isPresent();
+        if (store.isPresent()) {
+            return new ResponseEntity<>(true, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(false, HttpStatus.NOT_FOUND);
     }
 
     @PostMapping("/placeOrder")
-    public Map<String, String> placeOrder(@RequestBody PlaceOrderRequestDTO placeOrderRequest) {
+    public ResponseEntity<Map<String, String>> placeOrder(@RequestBody PlaceOrderRequestDTO placeOrderRequest) {
         Map<String, String> response = new HashMap<>();
         try {
             orderService.saveOrder(placeOrderRequest);
             response.put("message", "Pedido realizado con éxito");
+            return new ResponseEntity<>(response, HttpStatus.OK);
         } catch (Exception e) {
             response.put("Error", e.getMessage());
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
         }
-        return response;
     }
 }
